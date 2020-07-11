9da226c410e454d7c82ea03e5dee72703f04aa01
DjonyGH <counsil@mail.ru>
2020-07-11 10:49:26 +0700

    release with nativ js and animation with css

--- scripts/script-3.js
+++ scripts/script-3.js
@@ -3,14 +3,18 @@
 	dealList = document.querySelector('#deal-list'),
 	newdeaelDescription = document.querySelector('.newdeal-description'),
 	newdealBtn = document.querySelector('.newdeal-btn');
+	
 
-	let numDeal = 1;
+	let numDeal = 1,
+	startHeightDealElement = [];
+	const hideHeight = '60px';
 
 	const createNewDeal = () => {
-		console.log(newdealName.value.length)
 		if (newdealName.value.length > 0) {
 			if (newdeaelDescription.value.length > 0) {
-				dealList.insertAdjacentHTML('beforeend', '<div class="bg-white px-4 py-3 mb-4" id="deal-' + numDeal +'"><div class="mb-3"><h3 class="h4 font-weight-normal d-inline mr-1">' + newdealName.value + '</h3><button class="my-btn text-danger border-0 bg-white" id="close-btn-' + numDeal +'"><i class="fas fa-times"></i></button><button class="my-btn border-0 bg-white float-right" id="hide-btn-' + numDeal + '"><i class="fas fa-chevron-down"></i></button></div><p class="text-muted p-0 mt-3" id="deal-description">' + newdeaelDescription.value + '</p></div>');
+				dealList.insertAdjacentHTML('beforeend', '<div class="deal bg-white px-4 py-3 mb-4 " id="deal-' + numDeal +'"><div class="mb-3"><h3 class="h4 font-weight-normal d-inline mr-1">' + newdealName.value + '</h3><button class="my-btn text-danger border-0 bg-white" id="close-btn-' + numDeal +'"><i class="fas fa-times"></i></button><button class="my-btn border-0 bg-white float-right" id="hide-btn-' + numDeal + '"><i class="fas fa-chevron-down"></i></button></div><p class="text-muted p-0 mt-3" id="deal-description">' + newdeaelDescription.value + '</p></div>');
+				startHeightDealElement[numDeal] = getComputedStyle(document.querySelector(`#deal-${numDeal}`)).height;
+				document.querySelector(`#deal-${numDeal}`).style.height = startHeightDealElement[numDeal];
 				newdealName.value = '';
 				newdeaelDescription.value = '';
 				console.log('test1');
@@ -29,75 +33,36 @@
 				newdealName.classList.add('error');
 			}
 		}
+		
 	}
 
 	function removeClassError() {
 		this.classList.remove('error');
 	}
 
-	const deleteDeal = () => {
-
+	function deleteDeal() {
+		this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement);
 	}
 
-	const hideDeal = () => {
+	function hideDeal() {
+		this.firstChild.classList.toggle('fa-chevron-down');
+		this.firstChild.classList.toggle('fa-chevron-up');
+
+		console.log(+/\d+/.exec(this.id));
+		if(this.parentElement.parentElement.style.height == hideHeight) {
+			this.parentElement.parentElement.style.height = startHeightDealElement[+/\d+/.exec(this.id)];
+		} else this.parentElement.parentElement.style.height = hideHeight;
+
+		console.log(this.parentElement.parentElement.style.height);
 		
 	}
 
-	console.log(newdealName);
-	console.log(newdealBtn);
+
 	newdealBtn.addEventListener('click', createNewDeal);
 	newdealName.addEventListener('keydown', removeClassError);
 	newdeaelDescription.addEventListener('keydown', removeClassError);
 
+	console.log(startHeightDealElement);
+
 })();
 
-
-// $(function(){	
-// 	var numDeal = 1; //номер (первого) дела
-
-// 	function createNewDeal(){
-// 		if ($('.newdeal-name').val().length > 0) {		//Проверяем на наличие заголовка	
-// 			if ($('.newdeal-description').val().length > 0) {		//Проверяем на наличие описания	
-// 				$('#deal-list').append('<div class="bg-white px-4 py-3 mb-4" id="deal-' + numDeal +'"><div class="mb-3"><h3 class="h4 font-weight-normal d-inline mr-1">' + $('.newdeal-name').val() + '</h3><button class="my-btn text-danger border-0 bg-white" id="close-btn-' + numDeal +'"><i class="fas fa-times"></i></button><button class="my-btn border-0 bg-white float-right" id="hide-btn-' + numDeal + '"><i class="fas fa-chevron-down"></i></button></div><p class="text-muted p-0 mt-3" id="deal-description">' + $('.newdeal-description').val() + '</p></div>');
-// 				$('.newdeal-name').val(''); //очищаем поле ввода заголовка дела
-// 				$('.newdeal-description').val(''); // очищаем поле ввода описания дела
-// 				$('#close-btn-' + numDeal).on('click', deleteDeal); //создаем обработчик кнопки close для каждого дела
-// 				$('#hide-btn-' + numDeal).on('click', hideDeal); //создаем обработчик кнопки hide для каждого дела			
-// 				numDeal++; //увеличиваем номер для следющего дела
-// 			}
-// 			else { //при отсутствии описания делаем рамку поля ввода заголовка красной
-// 				$('.newdeal-description').addClass('error');
-// 			}
-// 		}  
-// 		else { //при отсутствии заголовка делаем рамку поля ввода заголовка красной
-// 			if ($('.newdeal-description').val().length > 0) {		//Проверяем на наличие описания	
-// 				$('.newdeal-name').addClass('error');
-// 			}
-// 			else { //при отсутствии описания делаем рамку поля ввода заголовка красной
-// 				$('.newdeal-description').addClass('error');
-// 				$('.newdeal-name').addClass('error');
-// 			}
-// 		}
-// 	};
-
-// 	function hideRedBorder(){
-// 		$(this).removeClass('error');
-// 	}
-
-// 	function deleteDeal(){
-// 		console.log('Кнопка ' + $(this).attr('id') + ' нажата');
-// 		$(this).parent().parent().remove(); //удаляем родительский контейнер кнопкм close, (this - это кнопка close, т.к. эту функцию вызвал обработчик этой кнопки)
-// 	};
-
-// 	function hideDeal(){
-// 		console.log('Кнопка ' + $(this).attr('id') + ' нажата');
-// 		$(this).children('i').toggleClass('fa-chevron-down fa-chevron-up'); //переворачиваем стрелку на кнопке
-// 		$(this).parent().siblings('#deal-description').slideToggle(); //скрываем контейнер с описанием дела
-// 	};
-
-		
-// 	$('.newdeal-btn').on('click', createNewDeal);
-// 	$('.newdeal-name').keydown(hideRedBorder);
-// 	$('.newdeal-description').keydown(hideRedBorder);
-
-// });
